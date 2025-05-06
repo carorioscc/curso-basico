e301ddcf6bb25c97914b62f3a9cdf412169701e4
carorioscc <carolina.rioslc@gmail.com>
2025-05-06 17:34:22 -0600

    Funcion para inicio de juego

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,1 +1,15 @@
-let botonMascotaJugador = document.getElementById('btn-mascota')
+//no poner guuion medio
+
+function iniciarJuego(){
+	let botonMascotaJugador = document.getElementById('btn-mascota')
+	botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+}
+
+function seleccionarMascotaJugador(){
+	alert('Sleccionaste tu mascota')
+}
+
+//cuando carga todo el html
+window.addEventListener('load', iniciarJuego)
+
+
