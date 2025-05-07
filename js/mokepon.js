5ad4dc119f6c7bc4d468f3d555f7220365b72f71
“Carolina <carolina.rioslc@gmail.com>
2025-05-06 23:08:39 -0600

    Funcion de seleccionar Jugador

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,14 +1,25 @@
-//no poner guuion medio
 
 function iniciarJuego(){
+    //no poner guuion medio a variables
 	let botonMascotaJugador = document.getElementById('btn-mascota')
 	botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 }
-
 function seleccionarMascotaJugador(){
-	alert('Sleccionaste tu mascota')
+    let inputBlacky=document.getElementById('blacky'),
+     inputCoco=document.getElementById('coco'),
+     inputCharly=document.getElementById('charly'),
+     spanMascotaJugador= document.getElementById('mascota-jugador'),
+     spanMacotaEnemigo = document.getElementById('mascota-enemigo');
+	if(inputBlacky.checked){
+        spanMascotaJugador.innerHTML='Blacky ';
+    }else if(inputCoco.checked){
+        spanMascotaJugador.innerHTML='Coco ';
+    }else if(inputCharly.checked){
+        spanMascotaJugador.innerHTML='Charly Brown ';
+    }else{
+        alert("Selecciona una mascota")
+    }
 }
-
 //cuando carga todo el html
 window.addEventListener('load', iniciarJuego)
 
